53b7f96b039be0c6d606af5c0fe8b838b53c4c42
Jacob Eliason <contact@jacobeliason.com>
2025-02-25 03:10:03 -0500

    Filled out project setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import os
 import sys
 
+import duckdb
+import pytest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+@pytest.fixture
+def conn():
+    with duckdb.connect(":memory:") as conn:
+        yield conn
